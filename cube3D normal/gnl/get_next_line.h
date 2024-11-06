c2d90e7afbaab3129fe2aab3431e59ef848089c8
Abdul Rashed Jamshidi <ajamshid@made-f0Ar7s6.clusters.42paris.fr>
2024-11-06 20:00:10 +0100

    add_gnl

---
+++ cube3D normal/gnl/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajamshid <ajamshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 12:33:27 by ajamshid          #+#    #+#             */
+/*   Updated: 2023/12/29 15:07:45 by ajamshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+void	ft_bzero(void *s);
+char	*ft_strdup(const char *s);
+
+#endif
